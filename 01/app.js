d4c7d06a6b7ca05bba4a53bd2b8de3b4529c6b70
Marcin Wabinski <wabinskim@gmail.com>
2024-12-30 06:08:45 +0100

    Finish task 01

--- 01/app.js
+++ 01/app.js
@@ -1,15 +1,29 @@
 function getProductList() {
-    return [
-        {name: 'JavaScript: podstawy', type: 'book', count: 3, price: 67.19, discount: 0.21},
-        {name: 'React: podstawy', type: 'book', count: 4, price: 79.17, discount: 0.27}
-    ]
+  return [
+    {
+      name: "JavaScript: podstawy",
+      type: "book",
+      count: 3,
+      price: 67.19,
+      discount: 0.21,
+    },
+    {
+      name: "React: podstawy",
+      type: "book",
+      count: 4,
+      price: 79.17,
+      discount: 0.27,
+    },
+  ];
 }
 
 function getTotalPrice(productList) {
-    return productList.reduce( (product, acc) => {
-        return acc + (product.price * product.count);
-    });
+  return productList
+    .reduce((acc, product) => {
+      return acc + product.price * product.count * (1 - product.discount);
+    }, 0)
+    .toFixed(2);
 }
 
-const totalPrice = getTotalPrice( getProductList() );
+const totalPrice = getTotalPrice(getProductList());
 console.log(totalPrice); // prawidłowa wartość: 390.42 (należy zaaokrąglić do 2 miejsc po przecinku)
