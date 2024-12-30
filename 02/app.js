f3ffd6d3ef68e950e8ca20d5cc330a56264ea51b
Marcin Wabinski <wabinskim@gmail.com>
2024-12-30 06:34:13 +0100

    Finish task 02

--- 02/app.js
+++ 02/app.js
@@ -1,28 +1,34 @@
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    const clickEl = document.querySelector('.error--click');
-    const enterEl = document.querySelector('.error--enter');
+  const clickEl = document.querySelector(".error--click");
+  const enterEl = document.querySelector(".error--enter");
 
-    setRandomPosition(clickEl);
-    setRandomPosition(enterEl);
+  setRandomPosition(clickEl);
+  setRandomPosition(enterEl);
 
-    initEventWithError(clickEl, 'click', new RangeError('Błąd zakresu!'));
-    initEventWithError(enterEl, 'mouseenter', new TypeError('Błąd typu!'));
-
+  initEventWithError(clickEl, "click", new RangeError("Błąd zakresu!"));
+  initEventWithError(enterEl, "mouseenter", new TypeError("Błąd typu!"));
 }
 
 function setRandomPosition(element, error = null) {
-    element.style.top = Math.random() * 600 + 'px';
-    element.style.left = Math.random() * 800 + 'px';
+  try {
+    element.style.top = Math.random() * 600 + "px";
+    element.style.left = Math.random() * 800 + "px";
 
-    if(error) {
-        throw error;
+    if (error) {
+      throw error;
     }
+  } catch (err) {
+    const alertEl = document.querySelector(".alert");
+    const alertMsg = alertEl.querySelector(".alert__message");
+    alertMsg.textContent = err.message;
+    alertEl.classList.remove("alert--hidden");
+  }
 }
 
 function initEventWithError(element, eventName, error) {
-    element.addEventListener(eventName, function() {
-        setRandomPosition(this, error);
-    })
+  element.addEventListener(eventName, function () {
+    setRandomPosition(this, error);
+  });
 }
