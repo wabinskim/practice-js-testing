8168c95315f8a1622ed705b8e4f7f34de4b12771
Marcin Wabinski <wabinskim@gmail.com>
2025-05-12 13:52:53 +0200

    Test 'should reject if first param is not given' added

--- 03/app.test.js
+++ 03/app.test.js
@@ -3,3 +3,7 @@ import randomNumber from "./app";
 it("should return 1 if both params are equal to 1", () => {
   expect(randomNumber(1, 1)).toEqual(1);
 });
+
+it("should reject if first param is not given", () => {
+  expect(() => randomNumber()).toThrow("First param must be given!");
+});
