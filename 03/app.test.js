6a73370778f67995aa4c30d0137e954e0e051d31
Marcin Wabinski <wabinskim@gmail.com>
2025-05-12 14:01:38 +0200

    Test 'should reject if second param is not given' added

--- 03/app.test.js
+++ 03/app.test.js
@@ -7,3 +7,7 @@ it("should return 1 if both params are equal to 1", () => {
 it("should reject if first param is not given", () => {
   expect(() => randomNumber()).toThrow("First param must be given!");
 });
+
+it("should reject if second param is not given", () => {
+  expect(() => randomNumber(1)).toThrow("Second param must be given!");
+});
