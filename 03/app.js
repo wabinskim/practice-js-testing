4a0129fd68438a0a215165a18d911289ea6443d2
Marcin Wabinski <wabinskim@gmail.com>
2025-05-22 13:43:26 +0200

    Type check for min param added

--- 03/app.js
+++ 03/app.js
@@ -1,9 +1,14 @@
 export default function randomNumber(min, max) {
+  //   max = Number(max);
   if (min === undefined) {
     throw new Error("First param must be given!");
   }
   if (max === undefined) {
     throw new Error("Second param must be given!");
+  }
+  min = Number(min);
+  if (typeof min !== "number" || !Number.isFinite(Number(min))) {
+    throw new Error("First param must be a number!");
   }
   return Math.random() * (max - min) + min;
 }
