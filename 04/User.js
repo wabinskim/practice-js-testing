663736b36465573ec383cd5d294df47790bd788b
Marcin Wabinski <wabinskim@gmail.com>
2025-06-12 12:16:21 +0200

    Finish task 04

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,34 @@
 export default class User {
+  constructor({ email, password }) {
+    if (!this._validateEmail(email)) {
+      throw new Error("Incorrect email!");
+    }
+    if (!this._validatePassword(password)) {
+      throw new Error("Incorrect password!");
+    }
+    this.email = email;
+    this.password = password;
+  }
 
+  getEmail() {
+    return this.email;
+  }
+
+  getPassword() {
+    return this.password;
+  }
+
+  login() {
+    return this.email.includes("devmentor.pl");
+  }
+
+  _validateEmail(email) {
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/;
+    return regex.test(email);
+  }
+
+  _validatePassword(password) {
+    const regex = /^[a-zA-Z]{2}\d{6}$/;
+    return regex.test(password);
+  }
 }
--- 04/User.test.js
+++ 04/User.test.js
@@ -1,46 +1,46 @@
-import User from './User';
+import User from "./User";
 
-describe('User', () => {
-    it('should had set email and password when create instance', () => {
-        const email = 'koder@devmentor.pl';
-        const password = 'pw123456';
-        const user = new User({email, password});
+describe("User", () => {
+  it("should had set email and password when create instance", () => {
+    const email = "koder@devmentor.pl";
+    const password = "pw123456";
+    const user = new User({ email, password });
 
-        expect(user.getEmail()).toBe(email);
-        expect(user.getPassword()).toBe(password);
+    expect(user.getEmail()).toBe(email);
+    expect(user.getPassword()).toBe(password);
+  });
+
+  it("should throw exception when email is incorrect", () => {
+    function createWrongUserData() {
+      new User({ email: "devmentor.pl", password: "pw123456" });
+    }
+
+    expect(createWrongUserData).toThrow();
+  });
+
+  it("should throw exception when password is incorrect", () => {
+    function createWrongUserData() {
+      new User({ email: "koder@devmentor.pl", password: "123" });
+    }
+
+    expect(createWrongUserData).toThrow();
+  });
+
+  describe(".login()", () => {
+    it("should return true when email contain domain devmentor.pl", () => {
+      const email = "koder@devmentor.pl";
+      const password = "pw123456";
+      const user = new User({ email, password });
+
+      expect(user.login()).toBe(true);
     });
 
-    it('should throw exception when email is incorrect', () => {
-        function createWrongUserData() {
-            new User({email: 'devmentor.pl', password: 'pw123456'});
-        }
+    it("should return false when email not contain domain  devmentor.pl", () => {
+      const email = "koder@gmail.com";
+      const password = "pw123456";
+      const user = new User({ email, password });
 
-        expect(createWrongUserData).toThrow();
+      expect(user.login()).toBe(false);
     });
-
-    it('should throw exception when password is incorrect', () => {
-        function createWrongUserData() {
-            new User({email: 'koder@devmentor.pl', password: '123'});
-        }
-
-        expect(createWrongUserData).toThrow();
-    });
-
-    describe('.login()', () => {
-        it('should return true when email contain domain devmentor.pl', () => {
-            const email = 'koder@devmentor.pl';
-            const password = 'pw123456';
-            const user = new User({email, password});
-
-            expect(user.login()).toBe(true);
-        });
-
-        it('should return false when email not contain domain  devmentor.pl', () => {
-            const email = 'koder@gmail.com';
-            const password = 'pw123456';
-            const user = new User({email, password});
-
-            expect(user.login()).toBe(false);
-        });
-    });
+  });
 });
